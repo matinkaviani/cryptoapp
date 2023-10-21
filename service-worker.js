f39f58c4a4cc1bdfded4d1438d36784a792702dc
Matin Kaviani <59339049+matinkaviani@users.noreply.github.com>
2023-10-21 23:23:59 +0330

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/cryptoapp/precache-manifest.9c67919ffd3dedbc1a6d4007e2e18f3c.js"
+  "/cryptoapp/precache-manifest.6decb910fcbdbbf6449ac76c3543f4e7.js"
 );
 
 workbox.clientsClaim();
