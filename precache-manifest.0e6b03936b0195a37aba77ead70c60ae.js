163f6d7dd31df06a0a07866bea829f02d1c564b4
Matin Kaviani <59339049+matinkaviani@users.noreply.github.com>
2023-10-21 21:54:24 +0330

    Updates

---
+++ precache-manifest.0e6b03936b0195a37aba77ead70c60ae.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = [
+  {
+    "revision": "8042a99a3abf641b30c9",
+    "url": "/cryptoapp/static/css/main.da62b63e.chunk.css"
+  },
+  {
+    "revision": "8042a99a3abf641b30c9",
+    "url": "/cryptoapp/static/js/main.8042a99a.chunk.js"
+  },
+  {
+    "revision": "e57e0f6c2b16432cd1f4",
+    "url": "/cryptoapp/static/css/1.33371a73.chunk.css"
+  },
+  {
+    "revision": "e57e0f6c2b16432cd1f4",
+    "url": "/cryptoapp/static/js/1.e57e0f6c.chunk.js"
+  },
+  {
+    "revision": "8fe4d9bb7554c353d9ee",
+    "url": "/cryptoapp/static/js/runtime~main.8fe4d9bb.js"
+  },
+  {
+    "revision": "138b0a9f8297c0bc6b9b1495baded929",
+    "url": "/cryptoapp/static/media/btcimg.138b0a9f.svg"
+  },
+  {
+    "revision": "0527cf8e7eae1a98d00b07704179b218",
+    "url": "/cryptoapp/static/media/logo.0527cf8e.svg"
+  },
+  {
+    "revision": "50016d7a55c63b2c795a3bc34c502e7b",
+    "url": "/cryptoapp/static/media/discordimg.50016d7a.svg"
+  },
+  {
+    "revision": "383ec4578a89a1e430515795b3b9ff59",
+    "url": "/cryptoapp/static/media/globeimg.383ec457.svg"
+  },
+  {
+    "revision": "fe39cff52b9b882d4786fc3dd0cc5a7f",
+    "url": "/cryptoapp/static/media/mimg.fe39cff5.svg"
+  },
+  {
+    "revision": "a717ee8490175ca06cf355831ccaac70",
+    "url": "/cryptoapp/static/media/lizardimg.a717ee84.svg"
+  },
+  {
+    "revision": "7370c3679472e9560965ff48a4399d0b",
+    "url": "/cryptoapp/static/media/roboto-latin-100.7370c367.woff2"
+  },
+  {
+    "revision": "f9e8e590b4e0f1ff83469bb2a55b8488",
+    "url": "/cryptoapp/static/media/roboto-latin-100italic.f9e8e590.woff"
+  },
+  {
+    "revision": "ef7c6637c68f269a882e73bcb57a7f6a",
+    "url": "/cryptoapp/static/media/roboto-latin-300.ef7c6637.woff2"
+  },
+  {
+    "revision": "5cb7edfceb233100075dc9a1e12e8da3",
+    "url": "/cryptoapp/static/media/roboto-latin-100.5cb7edfc.woff"
+  },
+  {
+    "revision": "f8b1df51ba843179fa1cc9b53d58127a",
+    "url": "/cryptoapp/static/media/roboto-latin-100italic.f8b1df51.woff2"
+  },
+  {
+    "revision": "b00849e00f4c2331cddd8ffb44a6720b",
+    "url": "/cryptoapp/static/media/roboto-latin-300.b00849e0.woff"
+  },
+  {
+    "revision": "14286f3ba79c6627433572dfa925202e",
+    "url": "/cryptoapp/static/media/roboto-latin-300italic.14286f3b.woff2"
+  },
+  {
+    "revision": "4df32891a5f2f98a363314f595482e08",
+    "url": "/cryptoapp/static/media/roboto-latin-300italic.4df32891.woff"
+  },
+  {
+    "revision": "51521a2a8da71e50d871ac6fd2187e87",
+    "url": "/cryptoapp/static/media/roboto-latin-400italic.51521a2a.woff2"
+  },
+  {
+    "revision": "479970ffb74f2117317f9d24d9e317fe",
+    "url": "/cryptoapp/static/media/roboto-latin-400.479970ff.woff2"
+  },
+  {
+    "revision": "fe65b8335ee19dd944289f9ed3178c78",
+    "url": "/cryptoapp/static/media/roboto-latin-400italic.fe65b833.woff"
+  },
+  {
+    "revision": "87284894879f5b1c229cb49c8ff6decc",
+    "url": "/cryptoapp/static/media/roboto-latin-500.87284894.woff"
+  },
+  {
+    "revision": "60fa3c0614b8fb2f394fa29944c21540",
+    "url": "/cryptoapp/static/media/roboto-latin-400.60fa3c06.woff"
+  },
+  {
+    "revision": "020c97dc8e0463259c2f9df929bb0c69",
+    "url": "/cryptoapp/static/media/roboto-latin-500.020c97dc.woff2"
+  },
+  {
+    "revision": "288ad9c6e8b43cf02443a1f499bdf67e",
+    "url": "/cryptoapp/static/media/roboto-latin-500italic.288ad9c6.woff"
+  },
+  {
+    "revision": "db4a2a231f52e497c0191e8966b0ee58",
+    "url": "/cryptoapp/static/media/roboto-latin-500italic.db4a2a23.woff2"
+  },
+  {
+    "revision": "2735a3a69b509faf3577afd25bdf552e",
+    "url": "/cryptoapp/static/media/roboto-latin-700.2735a3a6.woff2"
+  },
+  {
+    "revision": "da0e717829e033a69dec97f1e155ae42",
+    "url": "/cryptoapp/static/media/roboto-latin-700italic.da0e7178.woff2"
+  },
+  {
+    "revision": "adcde98f1d584de52060ad7b16373da3",
+    "url": "/cryptoapp/static/media/roboto-latin-700.adcde98f.woff"
+  },
+  {
+    "revision": "9b3766ef4a402ad3fdeef7501a456512",
+    "url": "/cryptoapp/static/media/roboto-latin-900.9b3766ef.woff2"
+  },
+  {
+    "revision": "81f57861ed4ac74741f5671e1dff2fd9",
+    "url": "/cryptoapp/static/media/roboto-latin-700italic.81f57861.woff"
+  },
+  {
+    "revision": "bb1e4dc6333675d11ada2e857e7f95d7",
+    "url": "/cryptoapp/static/media/roboto-latin-900.bb1e4dc6.woff"
+  },
+  {
+    "revision": "28f9151055c950874d2c6803a39b425b",
+    "url": "/cryptoapp/static/media/roboto-latin-900italic.28f91510.woff"
+  },
+  {
+    "revision": "ebf6d1640ccddb99fb49f73c052c55a8",
+    "url": "/cryptoapp/static/media/roboto-latin-900italic.ebf6d164.woff2"
+  },
+  {
+    "revision": "0f1f7ee3c45025f063adfc0d799ffcae",
+    "url": "/cryptoapp/index.html"
+  }
+];
